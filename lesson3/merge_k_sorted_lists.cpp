12ee80bcac71d71202298ddae4802d5760f1222e
My Name <my-name@chromium.org>
2021-10-18 16:51:04 +0800

    lesson3

--- lesson3/merge_k_sorted_lists.cpp
+++ lesson3/merge_k_sorted_lists.cpp
@@ -1,28 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
-        if ((!a) || (!b)) return a ? a : b;
-        ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
-        while (aPtr && bPtr) {
-            if (aPtr->val < bPtr->val) {
-                tail->next = aPtr; aPtr = aPtr->next;
-            } else {
-                tail->next = bPtr; bPtr = bPtr->next;
-            }
-            tail = tail->next;
-        }
-        tail->next = (aPtr ? aPtr : bPtr);
-        return head.next;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return doMerge(lists, 0 ,lists.size() -1);
     }
 
-    ListNode* merge(vector <ListNode*> &lists, int l, int r) {
+    ListNode* doMerge(vector <ListNode*> &lists, int l, int r) {
         if (l == r) return lists[l];
         if (l > r) return nullptr;
-        int mid = (l + r) >> 1;
-        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+        //分成兩份
+        int mid =  (l+r) >> 1;
+        return merge2Lists(doMerge(lists, l , mid),doMerge(lists, mid+ 1, r) );
     }
 
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        return merge(lists, 0, lists.size() - 1);
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) {
+            return l2;
+        } else if (l2 == nullptr) {
+            return l1;
+        } else if (l1->val < l2->val) {
+            l1->next = merge2Lists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge2Lists(l1, l2->next);
+            return l2;
+        }
     }
 };
